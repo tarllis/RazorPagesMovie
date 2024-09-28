0a6823d783f0dc25bef1872b072f5ec67586fb2f
Tarllis <tarllis@gmail.com>
2024-09-28 19:23:53 -0300

    Adicionar arquivos de projeto.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public string? Genero { get; set; }
+        public decimal Preço { get; set; }
+    }
+}
